e0d454e7c763ecffcfe06b95bf08b80bde325034
yuichkun <yuichkun@gmail.com>
2023-02-09 23:31:34 +0900

    create 2D range slider

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,59 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { draggable } from "@neodrag/svelte";
+  let x: number = 250;
+  let y: number = 250;
+  function normalizePos({ x, y }: { x: number; y: number }) {
+    return {
+      x: (x + 1) / 468,
+      y: (y + 1) / 468,
+    };
+  }
+  let normalizedX: number;
+  let normalizedY: number;
+  $: {
+    const normalizedPos = normalizePos({ x, y });
+    normalizedX = normalizedPos.x;
+    normalizedY = normalizedPos.y;
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <pre>
+    x: {normalizedX}
+    y: {normalizedY}
+  </pre>
+  <div id="parent">
+    <div
+      id="pointer"
+      use:draggable={{
+        bounds: "parent",
+        defaultPosition: {
+          x,
+          y,
+        },
+      }}
+      on:neodrag:start={(e) => console.log("Dragging started", e)}
+      on:neodrag={(e) => {
+        x = e.detail.offsetX;
+        y = e.detail.offsetY;
+      }}
+      on:neodrag:end={(e) => console.log("Dragging stopped", e)}
+    />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  #parent {
+    width: 500px;
+    height: 500px;
+    border: 1px white solid;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  #pointer {
+    width: 30px;
+    height: 30px;
+    background: white;
+    border: 2px rgb(193, 193, 193) solid;
+    border-radius: 50%;
+    cursor: pointer;
   }
 </style>
