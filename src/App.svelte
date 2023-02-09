bdc97dddab7a4f83b173aa03a3294f7c438b7c3f
yuichkun <yuichkun@gmail.com>
2023-02-10 01:12:37 +0900

    bind values

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { draggable } from "@neodrag/svelte";
-  import { createDevice } from "@rnbo/js";
+  import { createDevice, type Device } from "@rnbo/js";
   let x: number = 250;
   let y: number = 250;
   function normalizePos({ x, y }: { x: number; y: number }) {
@@ -9,73 +9,73 @@
       y: (y + 1) / 468,
     };
   }
-  let normalizedX: number;
-  let normalizedY: number;
-  $: {
-    const normalizedPos = normalizePos({ x, y });
-    normalizedX = normalizedPos.x;
-    normalizedY = normalizedPos.y;
-  }
-
   let WAContext = window.AudioContext || (window as any).webkitAudioContext;
   let context = new WAContext();
+  let device: Device | null = null;
+  $: isStarted = device !== null;
+
   const onClick = async () => {
     await context.resume();
     let rawPatcher = await fetch("patch.export.json");
     let patcher = await rawPatcher.json();
-    const device = await createDevice({ context, patcher });
-    device.node.connect(context.destination);
+    const _device = await createDevice({ context, patcher });
+    _device.node.connect(context.destination);
 
     const dependencies = await (await fetch("dependencies.json")).json();
 
-    const results = await device.loadDataBufferDependencies(dependencies);
-    results.forEach((result) => {
-      if (result.type === "success") {
-        console.log(`Successfully loaded buffer with id ${result.id}`, result);
-      } else {
-        console.log(
-          `Failed to load buffer with id ${result.id}, ${result.error}`
-        );
-      }
-    });
-    const pos = device.parametersById.get("pos");
-    pos.value = 0.4573;
-    console.log(device.parametersById.get("pos"));
-
-    const pitch = device.parametersById.get("pitch");
-    pitch.value = 0.7065;
-    console.log(device.parametersById.get("pitch"));
-
-    const play = device.parametersById.get("play");
-    play.value = 1;
-    console.log(device.parametersById.get("play"));
+    const results = await _device.loadDataBufferDependencies(dependencies);
+    const isSuccess = results.every((result) => result.type === "success");
+    if (isSuccess) {
+      device = _device;
+    }
   };
 </script>
 
 <main>
-  <button on:click={onClick}>button</button>
-  <pre>
-    x: {normalizedX}
-    y: {normalizedY}
-  </pre>
-  <div id="parent">
-    <div
-      id="pointer"
-      use:draggable={{
-        bounds: "parent",
-        defaultPosition: {
-          x,
-          y,
-        },
-      }}
-      on:neodrag:start={(e) => console.log("Dragging started", e)}
-      on:neodrag={(e) => {
-        x = e.detail.offsetX;
-        y = e.detail.offsetY;
-      }}
-      on:neodrag:end={(e) => console.log("Dragging stopped", e)}
-    />
-  </div>
+  {#if isStarted}
+    <pre>
+    x: {x}
+    y: {y}
+    </pre>
+    <div id="parent">
+      <div
+        id="pointer"
+        use:draggable={{
+          bounds: "parent",
+          defaultPosition: {
+            x,
+            y,
+          },
+        }}
+        on:neodrag:start={(e) => {
+          if (!device) return;
+          const play = device.parametersById.get("play");
+          play.value = 1;
+        }}
+        on:neodrag={(e) => {
+          if (!device) return;
+          const normalizedPos = normalizePos({
+            x: e.detail.offsetX,
+            y: e.detail.offsetY,
+          });
+          x = normalizedPos.x;
+          y = normalizedPos.y;
+
+          const pos = device.parametersById.get("pos");
+          const pitch = device.parametersById.get("pitch");
+          pos.value = x;
+          pitch.value = y;
+        }}
+        on:neodrag:end={(e) => {
+          if (!device) return;
+          const play = device.parametersById.get("play");
+          play.value = 0.0;
+        }}
+      />
+    </div>
+  {:else}
+    <button on:click={onClick}>button</button>
+  {/if}
 </main>
 
 <style>
@@ -91,5 +91,6 @@
     border: 2px rgb(193, 193, 193) solid;
     border-radius: 50%;
     cursor: pointer;
+    filter: blur(2px) brightness(200%);
   }
 </style>
