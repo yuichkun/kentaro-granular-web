18060eaf07e58f5ec8a618ebf9f1342f0d3a43c2
yuichkun <yuichkun@gmail.com>
2023-02-16 02:52:50 +0900

    enable saving with shortcut

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import { audioUrl, isAudioHidden, isRecording } from "./stores";
   let audioEl: HTMLAudioElement;
+  let downloadButton: HTMLAnchorElement;
+
+  const download = () => {
+    downloadButton.click();
+  };
 
   function onKeyDown(e: KeyboardEvent) {
     if (e.code === "Space" && !$isRecording) {
@@ -10,10 +15,17 @@
         audioEl.pause();
       }
     }
+
+    if (e.key === "s" && e.metaKey && !$isRecording) {
+      e.preventDefault();
+      download();
+    }
   }
 </script>
 
-<audio bind:this={audioEl} controls hidden={$isAudioHidden} src={$audioUrl} />
+<audio bind:this={audioEl} controls hidden={$isAudioHidden} src={$audioUrl}>
+  <a bind:this={downloadButton} href={$audioUrl} download>download</a>
+</audio>
 <svelte:window on:keydown={onKeyDown} />
 
 <style>
