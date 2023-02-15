309dda53d85e74a1e70f4588c7b11a9ed91886f0
yuichkun <yuichkun@gmail.com>
2023-02-15 18:03:49 +0900

    mv waveform inside record button

--- src/Granular.svelte
+++ src/Granular.svelte
@@ -4,63 +4,11 @@
   import type { RnboModule } from "./rnbo";
   import XYPad from "./XYPad.svelte";
   import type { ChangeBufferEventPayload } from "./types";
-  import { onMount } from "svelte";
-  import { createAnalyser } from "./createAnalyser";
-
   export let rnboModule: RnboModule;
   export let context: AudioContext;
 
-  const WIDTH = window.innerWidth;
-  const HEIGHT = window.innerHeight;
-  let canvas: HTMLCanvasElement;
   $: device = rnboModule.device;
   $: changeBuffer = rnboModule.changeBuffer;
-
-  onMount(() => {
-    const analyser = createAnalyser(context, device);
-
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    analyser.getByteTimeDomainData(dataArray);
-
-    const canvasCtx = canvas.getContext("2d");
-    if (!canvasCtx) return;
-
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-    function draw() {
-      requestAnimationFrame(draw);
-      if (!canvasCtx) return;
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-      analyser.getByteTimeDomainData(dataArray);
-      canvasCtx.fillStyle = "rgb(255, 255, 255, 0)";
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-      canvasCtx.lineWidth = 0.7;
-      canvasCtx.beginPath();
-      const sliceWidth = WIDTH / bufferLength;
-      let x = 0;
-      console.log(dataArray);
-      for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] / 128.0;
-        if (v === 1) {
-          canvasCtx.strokeStyle = "rgb(0, 0, 0, 0)";
-        } else {
-          canvasCtx.strokeStyle = "rgb(20,20,20, 0.75)";
-        }
-        const y = v * (HEIGHT / 2);
-        if (i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
-      }
-      canvasCtx.lineTo(WIDTH, HEIGHT / 2);
-      canvasCtx.stroke();
-    }
-    draw();
-  });
 
   let fileName: string = "Plaits_20200805_10.wav";
 
@@ -77,7 +25,6 @@
   };
 </script>
 
-<canvas width={WIDTH} height={HEIGHT} bind:this={canvas} />
 <XYPad {rnboModule} {fileName} on:changeBuffer={onChangeBuffer} />
 <div class="controls">
   <RecordButton {context} {device} />
@@ -85,13 +32,6 @@
 </div>
 
 <style>
-  canvas {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: WIDTH;
-    height: HEIGHT;
-  }
   .controls {
     margin-top: 18px;
     display: grid;
--- src/RecordButton.svelte
+++ src/RecordButton.svelte
@@ -2,19 +2,66 @@
   import type { Device } from "@rnbo/js";
   import type { IMediaRecorder } from "extendable-media-recorder";
   import { onMount } from "svelte";
+  import { createAnalyser } from "./createAnalyser";
   import { setupMediaRecorder } from "./mediaRecorder";
   import { isRecording, isAudioHidden, audioUrl } from "./stores";
 
   export let device: Device;
   export let context: AudioContext;
+  let canvas: HTMLCanvasElement;
 
   let mediaRecorder: IMediaRecorder;
+  const WIDTH = 164;
+  const HEIGHT = 42.19;
   onMount(async () => {
     const onStop = (blob: Blob) => {
       const url = URL.createObjectURL(blob);
       audioUrl.set(url);
     };
     mediaRecorder = await setupMediaRecorder({ context, device, onStop });
+    const analyser = createAnalyser(context, device);
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+
+    const canvasCtx = canvas.getContext("2d");
+    if (!canvasCtx) return;
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+      requestAnimationFrame(draw);
+      if (!canvasCtx) return;
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      analyser.getByteTimeDomainData(dataArray);
+      canvasCtx.fillStyle = "rgb(255, 255, 255, 0)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      canvasCtx.lineWidth = 0.7;
+      canvasCtx.beginPath();
+      const sliceWidth = WIDTH / bufferLength;
+      let x = 0;
+      console.log(dataArray);
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        if (v === 1) {
+          canvasCtx.strokeStyle = "rgb(0, 0, 0, 0)";
+        } else {
+          canvasCtx.strokeStyle = "rgb(255,255,255)";
+        }
+        const y = v * (HEIGHT / 2);
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+      canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+      canvasCtx.stroke();
+    }
+    draw();
   });
   const recordSound = () => {
     if (!$isRecording) {
@@ -34,14 +81,33 @@
   }
 </script>
 
-<button
-  id="record-button"
-  on:click={recordSound}
-  class={$isRecording ? "red" : ""}>RECORD OUTPUT</button
->
+<div id="record-button-container">
+  <canvas
+    width={WIDTH}
+    height={HEIGHT}
+    bind:this={canvas}
+    style={`visibility: ${$isRecording ? "initial" : "hidden"}`}
+  />
+  <button
+    id="record-button"
+    on:click={recordSound}
+    class={$isRecording ? "red" : ""}>RECORD OUTPUT</button
+  >
+</div>
 <svelte:window on:keydown={onKeyDown} />
 
 <style>
+  #record-button-container {
+    position: relative;
+  }
+  canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: WIDTH;
+    height: HEIGHT;
+    border-radius: 32px;
+  }
   #record-button {
     width: 164px;
     background: transparent;
